9927f4ab75e9abbe5b5e0c1a0eb57ba863d4b15d
Anton Björk <anton.bjork@umu.se>
2020-11-06 11:03:06 +0100

    wrote BEDDetailCodec to add "bed detail" file support to htsjdk

--- src/isoform/cellpile/TabixMaker.java
+++ src/isoform/cellpile/TabixMaker.java
@@ -4,19 +4,19 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.InputStream;
 import java.util.Iterator;
+
 import htsjdk.samtools.util.BlockCompressedOutputStream;
 import htsjdk.samtools.util.Interval;
 import htsjdk.tribble.AbstractFeatureReader;
 import htsjdk.tribble.FeatureReader;
 import htsjdk.tribble.TabixFeatureReader;
 import htsjdk.tribble.Tribble;
-import htsjdk.tribble.bed.BEDCodec;
 import htsjdk.tribble.bed.BEDFeature;
 import htsjdk.tribble.index.IndexFactory;
 import htsjdk.tribble.index.tabix.TabixIndex;
 
 
-public class MakeTabix {
+public class TabixMaker {
 
 	public static String bgzipBedFile(String fileIn) throws Exception {
 
@@ -40,27 +40,26 @@ public class MakeTabix {
 	
 	public static String indexBedFile(String bedFileName) throws Exception {
 		final File bedFile = new File(bedFileName);
-		final TabixIndex tabixIndex = IndexFactory.createTabixIndex(bedFile, new BEDCodec(), null);
+		final TabixIndex tabixIndex = IndexFactory.createTabixIndex(bedFile, new BEDDetailCodec(), null);
 		tabixIndex.writeBasedOnFeatureFile(bedFile);
 		final String indexFileName = Tribble.tabixIndexFile(bedFileName);
 		return indexFileName;
 	}
-
+	
 	
 	public static void main(String[] args) throws Exception {
 
-		// Make bgzipped file. Block gzipped files allows indexing and accessing
-		// portions of the data
-		// Thus, tabix wants this
-		// Didnt work when I used file ending .bgzip, but .gz does
-		String bedFileName = "C:\\Users\\anton\\java_projects\\isocounter\\data\\UP000005640_9606_lipid.bed";
-//		String bedBgzipFileName = "C:\\Users\\anton\\java_projects\\isocounter\\data\\UP000005640_9606_lipid.bed.gz";
-		String bedBgzipFileName = MakeTabix.bgzipBedFile(bedFileName);
-
+		// Make bgzipped file. Block gzipped files allows indexing and accessing portions of the data.
+		// Thus, tabix wants this.
+//		// Didnt work when I used file ending .bgzip, but .gz does.
+//		String bedFileName = "C:\\Users\\anton\\java_projects\\isocounter\\data\\beds\\UP000005640_9606_lipid.bed";
+		String bedFileName = "C:\\Users\\anton\\java_projects\\isocounter\\data\\beds\\merged_all.sorted.bed_details_compatible.bed";
+		String bedBgzipFileName = TabixMaker.bgzipBedFile(bedFileName);
+		
 		// Make tabix index for bed file
-		String indexFileName = MakeTabix.indexBedFile(bedFileName);
+//		String indexFileName = TabixMaker.indexBedFile(bedFileName);
 //		System.out.println(indexFileName);
-		String indexBgzipFileName = MakeTabix.indexBedFile(bedBgzipFileName);
+		String indexBgzipFileName = TabixMaker.indexBedFile(bedBgzipFileName);
 //		System.out.println(indexBgzipFileName);
 
 		// Query Tabix		
@@ -69,29 +68,27 @@ public class MakeTabix {
 //		System.out.println(Tribble.indexFile(bedBgzipFileName));
 //		System.out.println(Tribble.tabixIndexFile(bedBgzipFileName));
 		
-		final FeatureReader<BEDFeature> reader = AbstractFeatureReader.getFeatureReader(bedBgzipFileName, new BEDCodec());
-//		final FeatureReader<BEDFeature> readerUncompressed = AbstractFeatureReader.getFeatureReader(bedFileName, new BEDCodec());
-//		final FeatureReader<BEDFeature> reader = AbstractFeatureReader.getFeatureReader(bedFileName, new BEDCodec());
+		final FeatureReader<BEDFeature> reader = AbstractFeatureReader.getFeatureReader(bedBgzipFileName, new BEDDetailCodec());
 
+		// cd55 location
 		Interval interval = new Interval("chr1", 207321376, 207360966);
-		System.out.println(interval.getContig());
-		System.out.println(interval.getStart());
-		System.out.println(interval.getEnd());
 		
 		final Iterator<BEDFeature> readerIterator = 
 				reader.query(interval.getContig(), interval.getStart(), interval.getEnd());
 		while (readerIterator.hasNext()) {
 			BEDFeature bedFeature = readerIterator.next();
-			System.out.println(bedFeature);
+//			System.out.println(bedFeature);
 //			System.out.println(bedFeature.getColor());
 			System.out.println(bedFeature.getDescription());
 //			System.out.println(bedFeature.getExons());
-//			System.out.println(bedFeature.getLink());
-			System.out.println(bedFeature.getName());
+			System.out.println(bedFeature.getLink());
+//			System.out.println(bedFeature.getName());
 //			System.out.println(bedFeature.getScore());
 //			System.out.println(bedFeature.getStrand());
 //			System.out.println(bedFeature.getType());
-			
+//			System.out.println(bedFeature.getStart());
+//			System.out.println(bedFeature.getEnd());
+			System.out.println();
 		}
 
 	}
