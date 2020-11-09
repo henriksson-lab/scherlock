326b69b1815eae71967b96d5fd2779edeb5d7dd8
Anton Björk <anton.bjork@umu.se>
2020-11-09 17:11:45 +0100

    Made separate test scripts for Anton and Johan, so we dont have to change the file paths all the time

--- src/isoform/cellpile/CellPileTestReadJohan.java
+++ src/isoform/cellpile/CellPileTestReadJohan.java
@@ -16,7 +16,7 @@ import isoform.util.GtfParser;
  * @author Johan Henriksson
  *
  */
-public class CellPileTestRead {
+public class CellPileTestReadJohan {
 	
 	public static void main(String[] args) throws IOException {
 		
