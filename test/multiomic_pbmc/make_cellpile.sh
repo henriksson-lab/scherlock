18ea242812ccd46dda5f14a2fd0c729fb4b23212
Johan Henriksson <mahogny@areta.org>
2020-12-13 19:20:04 +0100

    test multiome; also build the complete files

--- test/multiomic_pbmc/make_cellpile.sh
+++ test/multiomic_pbmc/make_cellpile.sh
@@ -1,11 +1,23 @@
+######################## reduced ###########################################
 java -jar ../../build/cellpile.jar build \
 	reduced/10x_atac.cellpile \
 	sizes.chromosome \
-	reduced/atac_possorted_genome_bam.bam barcodes.tsv.gz
+	reduced/atac_possorted_genome_bam.bam reduced/barcodes.tsv.gz
 
 
 java -jar ../../build/cellpile.jar build \
 	reduced/10x_gex.cellpile \
 	sizes.chromosome \
-	reduced/gex_possorted_genome_bam.bam barcodes.tsv.gz
+	reduced/gex_possorted_genome_bam.bam reduced/barcodes.tsv.gz
 
+######################## original ###########################################
+java -jar ../../build/cellpile.jar build \
+	orig/10x_atac.cellpile \
+	sizes.chromosome \
+	orig/atac/possorted_genome_bam.bam orig/barcodes.tsv.gz
+
+
+java -jar ../../build/cellpile.jar build \
+	reduced/10x_gex.cellpile \
+	sizes.chromosome \
+	orig/gex/possorted_genome_bam.bam orig/barcodes.tsv.gz
--- test/multiomic_pbmc/make_isocount.sh
+++ test/multiomic_pbmc/make_isocount.sh
@@ -1,5 +1,8 @@
 #Define gene features
 java -jar ../../build/isocount.jar build reduced/ref/genes.gtf.gz reduced/featurefile.ff
 
-#Build the count table
+#Build the count table (reduced)
 java -jar ../../build/isocount.jar count reduced/isocount.gex reduced/featurefile.ff reduced/gex_possorted_genome_bam.bam barcodes.tsv.gz
+
+#Build the count table (whole)
+java -jar ../../build/isocount.jar count orig/isocount.gex reduced/featurefile.ff orig/gex/possorted_genome_bam.bam barcodes.tsv.gz
