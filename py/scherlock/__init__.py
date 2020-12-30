60f28f385ee3cb2a7daccb2e57025b3b2ad63dfc
Johan Henriksson <mahogny@areta.org>
2020-12-30 20:03:22 +0100

    test of new java bridge

--- py/scherlock/__init__.py
+++ py/scherlock/__init__.py
@@ -1,4 +1,5 @@
 
 from . import CellPile as cp
+from . import NewCellPile as ncp
 from . import plot as pl
 from . import stats as stats
---
+++ test/multiomic_pbmc/test_de.py
@@ -0,0 +1,20 @@
+#Load annotated counts
+
+
+#Load raw counts
+
+
+#Transfer annotation to raw counts
+
+
+#Do some DE tests
+
+
+
+#Plot some DE results
+
+#plot_vulcan(test_summary, min_pval=1, crop_pval=1e-20)
+#plot_vulcan(test_summary, min_pval=1, crop_pval=1e-30, groupby="leiden")
+
+
+
