66bf39c12b43f2f9dc9e3f21be3674cc682cac54
Johan Henriksson <mahogny@areta.org>
2021-01-05 10:10:52 +0100

    added python deps

--- py/setup.py
+++ py/setup.py
@@ -13,6 +13,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/henriksson-lab/isocounter",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'pandasql','scanpy','anndata','jnius','IPython',
+        'matplotlib','plotly','numpy','pandas'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
@@ -23,3 +27,4 @@ setuptools.setup(
     include_package_data=True,
     package_data={'': ['data/*.jar']},
 )
+
