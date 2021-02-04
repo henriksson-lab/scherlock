e6385825789d99a86bb6b0aa09d60ae7c263fd74
anbjork <anton.bjork@umu.se>
2021-02-04 18:37:41 +0100

    Added compilation script to test read and test write in src/isoform/cellpile/test

---
+++ src/isoform/cellpile/test/CellPileTestWrite.java
@@ -0,0 +1,61 @@
+package isoform.cellpile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import isoform.util.PileUtil;
+import isoform.cellpile.CellPileFile;
+
+/**
+ * Test functions
+ * 
+ * @author Johan Henriksson
+ *
+ */
+public class CellPileTestWrite {
+	
+	public static void main(String[] args) throws IOException {
+		
+		
+		String bamType="--single_cell";
+		File fCellpile=new File("/home/mahogny/temp/cellpile");
+		File fBAM=new File("/big/henriksson/tonsil/possorted_genome_bam.bam");
+		File fChromSizes=new File("/data/henlab/ref_genome/human/sizes.genome");
+		File fBC=new File("/big/henriksson/tonsil/filtered_feature_bc_matrix/barcodes.tsv.gz");
+		
+		ArrayList<String> listBarcodes=PileUtil.readBarcodeZipList(fBC);
+		
+		CellPileFile cp=CellPileFile.writeFile(fCellpile, fChromSizes, fBAM, bamType, listBarcodes);
+		cp.close();
+		
+		System.out.println("File written! ------ ");
+		
+		/*
+		cp=CellPileFile.open(fCellpile);
+		
+		int[][] clusters=cp.convertBarcodeNamesToIDs(
+				new String[][] {
+						cp.getListBarcodes().toArray(new String[0])
+				});
+		
+		Pileup pileup=cp.buildPileup(
+				"1", 1, 100000, 
+				100, 
+				clusters);
+		
+		System.out.println("");
+		System.out.println(pileup.toSVG());
+		*/
+		/*
+		for(int[] track:pileup.tracks) {
+			for(int i:track)
+				System.out.print(i+" ");
+			
+			System.out.println();
+		}*/
+		
+		
+	}
+
+}
---
+++ src/isoform/cellpile/test/compile_test_write_and_test_read.sh
@@ -0,0 +1,6 @@
+
+
+javac -cp /corgi/anbjork/scherlock/build/cellpile.jar CellPileTestWrite.java
+javac -cp /corgi/anbjork/scherlock/build/cellpile.jar CellPileTestReadJohan.java
+javac -cp /corgi/anbjork/scherlock/build/cellpile.jar CellPileTestReadAnton.java
+
