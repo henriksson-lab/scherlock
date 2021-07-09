7395993c0b138de416de901f6d754e33c2515b7f
anbjork <anton.bjork@umu.se>
2021-07-09 21:14:23 +0200

    automated testing more. Now beginning to look like a real test suite. All the results look good too, no bugs seen! :)

--- test/multiomic_pbmc/make_cellpile.sh
+++ test/multiomic_pbmc/make_cellpile.sh
@@ -5,23 +5,42 @@ sd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "$sd"
 
 
-######################## reduced ###########################################
+######################## reduced data ###########################################
+
 java -jar ../../build/cellpile.jar build \
 	'--single_cell' \
-	reduced/10x_atac.cellpile \
+	reduced/10x_atac_single_cell.cellpile \
 	sizes.chromosome \
 	reduced/atac_possorted_genome_bam.bam \
 	reduced/barcodes.tsv.gz
 
+java -jar ../../build/cellpile.jar build \
+	'--single_cell' \
+	reduced/10x_gex_single_cell.cellpile \
+	sizes.chromosome \
+	reduced/gex_possorted_genome_bam.bam \
+	reduced/barcodes.tsv.gz
 
-# java -jar ../../build/cellpile.jar build \
-# 	'--single_cell' \
-# 	reduced/10x_gex.cellpile \
-# 	sizes.chromosome \
-# 	reduced/gex_possorted_genome_bam.bam \
-# 	reduced/barcodes.tsv.gz
+java -jar ../../build/cellpile.jar build \
+	'--bulk' \
+	reduced/10x_atac_bulk.cellpile \
+	sizes.chromosome \
+	reduced/atac_possorted_genome_bam.bam \
+	'bulk_sample'
 
-# ######################## original ###########################################
+java -jar ../../build/cellpile.jar build \
+	'--bulk' \
+	reduced/10x_gex_bulk.cellpile \
+	sizes.chromosome \
+	reduced/gex_possorted_genome_bam.bam \
+	'bulk_sample'
+
+
+
+
+
+
+# ######################## fullsize data ###########################################
 # java -jar ../../build/cellpile.jar build \
 # 	fullsize/10x_atac.cellpile \
 # 	sizes.chromosome \
--- test/multiomic_pbmc/raw_test_cellpile2.py
+++ test/multiomic_pbmc/raw_test_cellpile2.py
@@ -9,36 +9,183 @@ adata.var_names_make_unique()
 adata.obs["sampleid"] = "sample1"
 adata.obs["leiden"] = "all"
 
-# Initialize cellpile
-cp = cpl.cellpile()
-
-### Load the reference genome
-cp.add_gtf("reduced/ref/genes.gtf.gz")
-# Note that the pilename must correspond to sample id
-cp.add_cellpile("reduced/10x_atac.cellpile", cellpile_name='sample1')  
-
-### Check our favourite gene
-v = cp.get_view("CD55")
-
-# Plot and save
-pathlib.Path("outs/").mkdir(parents=True, exist_ok=True)
-
-testing_order = [[False, False],
-                 [False, True ],
-                 [True,  False],
-                 [True,  True ]]
 
 
-for line in testing_order:
-    testing_args = {"show_inbetweens" : line[0],
-                    "individual_track_scaling" : line[1]}
-    file_name_midfix = "".join([key + '_' + str(value).ljust(5, '_') + '___' 
-        for key, value in testing_args.items()]).strip('_')
-    cp.pileup(v,
-              barcodes=adata.obs["origbc"].tolist(),      # subset on cell BCs
-              cellpile_names=adata.obs["sampleid"].tolist(),
-              track_labels=adata.obs["leiden"].tolist(),  # Split into different tracks for clusters
-              show_inbetweens=line[0],
-              individual_track_scaling=line[1],
-             ).plot(save="outs/cd55__" + file_name_midfix + ".svg")
+
+
+##################### Several tracks ######################### 
+
+# Several tracks based on cell clusters is neccessarily single cell
+mode = 'several_clusters'
+
+aa = ['asdf' for elem in range(round(adata.shape[0]/2))]
+bb = ['qwer' for elem in range(adata.shape[0] - round(adata.shape[0]/2))]
+fake_clusters = aa + bb
+assert(len(fake_clusters) == adata.shape[0])
+
+for dataType in ['gex', 'atac']:
+
+    outdir = "outs/" + mode + '/' + dataType
+
+    # Initialize cellpile
+    cp = cpl.cellpile()
+
+    ### Load the reference genome
+    cp.add_gtf("reduced/ref/genes.gtf.gz")
+    # Note that the pilename must correspond to sample id
+    cellpile_file = "reduced/10x_" + dataType + "_single_cell.cellpile"
+    cp.add_cellpile(cellpile_file, cellpile_name='sample1')
+
+    ### Check our favourite gene
+    v = cp.get_view("CD55")
+
+    # Plot and save
+    pathlib.Path(outdir).mkdir(parents=True, exist_ok=True)
+
+    testing_order = [[False, False],
+                     [False, True ],
+                     [True,  False],
+                     [True,  True ]]
+
+    for line in testing_order:
+        testing_args = {"show_inbetweens" : line[0],
+                        "individual_track_scaling" : line[1]}
+        file_name_midfix = "".join([key + '_' + str(value).ljust(5, '_') + '___' 
+            for key, value in testing_args.items()]).strip('_')
+        cp.pileup(v,
+                  barcodes=adata.obs["origbc"].tolist(),      # subset on cell BCs
+                  cellpile_names=adata.obs["sampleid"].tolist(),
+                  track_labels=fake_clusters,  # Split into different tracks for clusters
+                  show_inbetweens=line[0],
+                  individual_track_scaling=line[1],
+                 ).plot(save=(outdir + "/cd55__" + file_name_midfix + ".svg"))
+
+    del cp  # Make sure the old cellpile object is not lingering
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##################### Single cell ######################### 
+
+mode = 'single_cell'
+
+for dataType in ['gex', 'atac']:
+
+    outdir = "outs/" + mode + '/' + dataType
+
+    # Initialize cellpile
+    cp = cpl.cellpile()
+
+    ### Load the reference genome
+    cp.add_gtf("reduced/ref/genes.gtf.gz")
+    # Note that the pilename must correspond to sample id
+    cellpile_file = "reduced/10x_" + dataType + "_single_cell.cellpile"
+    cp.add_cellpile(cellpile_file, cellpile_name='sample1')
+
+    ### Check our favourite gene
+    v = cp.get_view("CD55")
+
+    # Plot and save
+    pathlib.Path(outdir).mkdir(parents=True, exist_ok=True)
+
+    testing_order = [[False, False],
+                     [False, True ],
+                     [True,  False],
+                     [True,  True ]]
+
+    for line in testing_order:
+        testing_args = {"show_inbetweens" : line[0],
+                        "individual_track_scaling" : line[1]}
+        file_name_midfix = "".join([key + '_' + str(value).ljust(5, '_') + '___' 
+            for key, value in testing_args.items()]).strip('_')
+        cp.pileup(v,
+                  barcodes=adata.obs["origbc"].tolist(),      # subset on cell BCs
+                  cellpile_names=adata.obs["sampleid"].tolist(),
+                  track_labels=adata.obs["leiden"].tolist(),  # Split into different tracks for clusters
+                  show_inbetweens=line[0],
+                  individual_track_scaling=line[1],
+                 ).plot(save=(outdir + "/cd55__" + file_name_midfix + ".svg"))
+
+    del cp  # Make sure the old cellpile object is not lingering
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##################### Bulk ######################### 
+
+
+mode = 'bulk'
+
+for dataType in ['gex', 'atac']:
+
+    outdir = "outs/" + mode + '/' + dataType
+
+    # Initialize cellpile
+    cp = cpl.cellpile()
+
+    ### Load the reference genome
+    cp.add_gtf("reduced/ref/genes.gtf.gz")
+    # Note that the pilename must correspond to sample id
+    cellpile_file = "reduced/10x_" + dataType + "_bulk.cellpile"
+    cp.add_cellpile(cellpile_file, cellpile_name='sample1')
+
+    ### Check our favourite gene
+    v = cp.get_view("CD55")
+
+    # Plot and save
+    pathlib.Path(outdir).mkdir(parents=True, exist_ok=True)
+
+    testing_order = [[False, False],
+                     [False, True ],
+                     [True,  False],
+                     [True,  True ]]
+
+    for line in testing_order:
+        testing_args = {"show_inbetweens" : line[0],
+                        "individual_track_scaling" : line[1]}
+        file_name_midfix = "".join([key + '_' + str(value).ljust(5, '_') + '___' 
+            for key, value in testing_args.items()]).strip('_')
+        cp.pileup(v,
+                  barcodes=["bulk_sample" for elem in range(adata.shape[0])],
+                  cellpile_names=adata.obs["sampleid"].tolist(),
+                  track_labels=adata.obs["leiden"].tolist(),
+                  show_inbetweens=line[0],
+                  individual_track_scaling=line[1],
+                 ).plot(save=(outdir + "/cd55__" + file_name_midfix + ".svg"))
+
+    del cp  # Make sure the old cellpile object is not lingering
+
+
+
+
+
 
